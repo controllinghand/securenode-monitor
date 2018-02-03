42b6fc12acc3c94649e7df0bc46c9c10bd052a76
controllinghand <31561227+controllinghand@users.noreply.github.com>
2018-02-03 14:37:08 -0800

    Update installsnma.sh

--- installsnma.sh
+++ installsnma.sh
@@ -41,5 +41,5 @@ wget https://rawgit.com/controllinghand/smartnode-monitor/master/snmonagent.sh
 
 # Create a cronjob for monitoring agent to collect data every 10 minutes
 # dump the results into the smartadmin snmon directory
-(crontab -l 2>/dev/null | grep -v -F "snmon/snmonagent.sh" ; echo "*/10 * * * * ~/snmon/snmonagent.sh" > /home/smartadmin/snmon/snmon.dat ) | crontab -
+(crontab -l 2>/dev/null | grep -v -F "snmon/snmonagent.sh" ; echo "*/10 * * * * ~/snmon/snmonagent.sh > /home/smartadmin/snmon/snmon.dat" ) | crontab -
 chmod 0700 ./snmonagent.sh
