fef2bfc616ef13d9e1927411143347e45f88fc21
controllinghand <31561227+controllinghand@users.noreply.github.com>
2018-02-03 14:35:55 -0800

    Update installsnma.sh

--- installsnma.sh
+++ installsnma.sh
@@ -22,6 +22,17 @@ cd
 rm -r snmon
 mkdir snmon
 
+# Check to see that smartadmin home dir exist
+if [[ ! -d /home/smartadmin ]]
+then
+     echo "Smartadmin directory does not exist. Exiting"
+     exit -1
+fi
+
+# Create the directory in smartadmin home
+rm -r /home/smartadmin/snmon
+mkdir /home/smartadmin/snmon
+
 # Change the directory to ~/snmon/
 cd ~/snmon/
 
@@ -29,5 +40,6 @@ cd ~/snmon/
 wget https://rawgit.com/controllinghand/smartnode-monitor/master/snmonagent.sh
 
 # Create a cronjob for monitoring agent to collect data every 10 minutes
-(crontab -l 2>/dev/null | grep -v -F "snmon/snmonagent.sh" ; echo "*/10 * * * * ~/snmon/snmonagent.sh" ) | crontab -
+# dump the results into the smartadmin snmon directory
+(crontab -l 2>/dev/null | grep -v -F "snmon/snmonagent.sh" ; echo "*/10 * * * * ~/snmon/snmonagent.sh" > /home/smartadmin/snmon/snmon.dat ) | crontab -
 chmod 0700 ./snmonagent.sh
