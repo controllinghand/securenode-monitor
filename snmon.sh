0907995a38c5df506f7109654e50cd85e6a567db
controllinghand <31561227+controllinghand@users.noreply.github.com>
2018-02-03 23:39:24 -0800

    Update snmon.sh

--- snmon.sh
+++ snmon.sh
@@ -51,22 +51,41 @@ DIFF=$((todayUTC-vpsdate))
 if [[ $DIFF -gt 72000 ]]
 then
     echo -en "[${RED}FAILED${NC}]Data is older than 20mins check VPS"
+    continue
 fi
 
 # Print out hostname
 hostname=$(echo "$DATA" | grep hostname | awk -F':' '{print $2}')
+if [[ ! $hostname ]]
+then
+    echo -en "[${RED}FAILED${NC}]no hostname found"
+    continue
+fi
 echo -en "[${GRN}OK${NC}]hostname: ${BLU}$hostname${NC}"
 echo ""
 
 # Check to see if smartcashd is running and by which user
 smartcashduser=$(echo "$DATA" | grep smartcashduser | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]smartcashd: ${BLU}$smartcashduser${NC} is running the application"
-echo ""
+if [[ ! $smartcashduser ]]
+then
+    echo -en "[${RED}FAILED${NC}]smartcashd is not running"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]smartcashd: ${BLU}$smartcashduser${NC} is running the application"
+    echo ""
+fi
 
 # Check smartnode status
 smartnodestatus=$(echo "$DATA" | grep smartnodestatus | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]status: ${BLU}$smartnodestatus${NC}"
-echo ""
+juststatus=$(echo $smartnodestatus | awk '{print $2}')
+if [[  "$juststatus" != "successfully" ]]
+then
+    echo -en "[${RED}FAILED${NC}]smartcashd is not running"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]status: ${BLU}$smartnodestatus${NC}"
+    echo ""
+fi
 
 # Check OS version 
 osversion=$(echo "$DATA" | grep osversion | awk -F':' '{print $2}')
@@ -91,54 +110,115 @@ echo ""
 
 # Check Disk Space 
 currentdiskspaceused=$(echo "$DATA" | grep currentdiskspaceused | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]current diskspace used%: ${BLU}$currentdiskspaceused${NC}"
-echo ""
+disknum=$(echo $currentdiskspaceused | awk -F'%' '{print $1}')
+if [[  $disknum -gt 90 ]]
+then
+    echo -en "[${RED}FAILED${NC}] $currentdiskspaceused over 90% check VPS for disk space"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]current diskspace used%: ${BLU}$currentdiskspaceused${NC}"
+    echo ""
+fi
 
 # Check ufw Firewall 
 ufwstatus=$(echo "$DATA" | grep ufwstatus | awk -F':' '{print $3}')
-echo -en "[${GRN}OK${NC}]firewall status: ${BLU}$ufwstatus${NC}"
-echo ""
+if [[  "$ufwstatus" != " active" ]]
+then
+    echo -en "[${RED}FAILED${NC}]firewall is not active"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]firewall status: ${BLU}$ufwstatus${NC}"
+    echo ""
+fi
 
 # Check ufw Firewall ssh
 ufwssh=$(echo "$DATA" | grep ufwssh | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]firewall ssh 22: ${BLU}$ufwssh${NC}"
-echo ""
+if [[  "$ufwssh" != "LIMIT" ]]
+then
+    echo -en "[${RED}FAILED${NC}]check firewall ssh 22 port settings"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]firewall ssh 22: ${BLU}$ufwssh${NC}"
+    echo ""
+fi
 
 # Check ufw Firewall smartcashd port 9678
 ufwscport=$(echo "$DATA" | grep ufwscport | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]firewall smartcashd 9768: ${BLU}$ufwscport${NC}"
-echo ""
+if [[  "$ufwscport" != "ALLOW" ]]
+then
+    echo -en "[${RED}FAILED${NC}]check firewall smartcashd 9768 port settings"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]firewall smartcashd 9768: ${BLU}$ufwscport${NC}"
+    echo ""
+fi
 
-# Check ufw Firewall smartcashd port 9678
+# Check ufw Firewall if any other ports are open 
 ufwother=$(echo "$DATA" | grep ufwother | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]firewall any other open: ${BLU}$ufwother${NC}"
-echo ""
+if [[ "$ufwother" != "none" ]]
+then
+    echo -en "[${RED}FAILED${NC}]$ufwother is open please close"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]firewall any other open: ${BLU}$ufwother${NC}"
+    echo ""
+fi
 
 # Check crontab jobs 
 # Check for makerun.sh  
 makerun=$(echo "$DATA" | grep makerun | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]makerun cronjob: ${BLU}$makerun${NC}"
-echo ""
+if [[ ! $makerun ]]
+then
+    echo -en "[${RED}FAILED${NC}]makerun cron job missing"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]makerun cronjob: ${BLU}$makerun${NC}"
+    echo ""
+fi
 
 # Check for checkdaemon.sh  
 checkdaemon=$(echo "$DATA" | grep checkdaemon | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]checkdaemon cronjob: ${BLU}$checkdaemon${NC}"
-echo ""
+if [[ ! $checkdaemon ]]
+then
+    echo -en "[${RED}FAILED${NC}]checkdaemon cron job missing"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]checkdaemon cronjob: ${BLU}$checkdaemon${NC}"
+    echo ""
+fi
 
 # Check for upgrade.sh  
 upgrade=$(echo "$DATA" | grep upgrade | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]upgrade cronjob: ${BLU}$upgrade${NC}"
-echo ""
+if [[ ! $upgrade ]]
+then
+    echo -en "[${RED}FAILED${NC}]upgrade cron job missing"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]upgrade cronjob: ${BLU}$upgrade${NC}"
+    echo ""
+fi
 
 # Check for clearlog.sh  
 clearlog=$(echo "$DATA" | grep clearlog | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]clearlog cronjob: ${BLU}$clearlog${NC}"
-echo ""
+if [[ ! $clearlog ]]
+then
+    echo -en "[${RED}FAILED${NC}]clearlog cron job missing"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]clearlog cronjob: ${BLU}$clearlog${NC}"
+    echo ""
+fi
 
 # Check for snmonagent.sh  
 snmonagent=$(echo "$DATA" | grep snmonagent | awk -F':' '{print $2}')
-echo -en "[${GRN}OK${NC}]snmonagent cronjob: ${BLU}$snmonagent${NC}"
-echo ""
+if [[ ! $snmonagent ]]
+then
+    echo -en "[${RED}FAILED${NC}]snmonagent cron job missing"
+    echo ""
+else
+    echo -en "[${GRN}OK${NC}]snmonagent cronjob: ${BLU}$snmonagent${NC}"
+    echo ""
+fi
 
 echo ""
 
