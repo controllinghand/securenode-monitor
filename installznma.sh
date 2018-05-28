9b6995e2409cc6330f97174e738e9853ba848b36
controllinghand <s@controllinghand.com>
2018-05-28 14:31:04 -0700

    update to zen
    changed from smart to zen

--- installznma.sh
+++ installznma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# installsnma.sh
-# This installs the SmartNode Monitor Agent on the VPS server
+# installznma.sh
+# This installs the ZenCash SecureNode Monitor Agent on the VPS server
 # Make sure to install this as root only
 
 if [ "$(whoami)" != "root" ]; then
@@ -9,9 +9,9 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 # Check to see if smartnode directory already exist
-if [ -d ~/snmon ]
+if [ -d ~/znmon ]
 then
-    echo "~/snmon Directory already exist"
+    echo "~/znmon Directory already exist"
     printf "Press Ctrl+C to cancel or Enter to clean up and reinstall:"
     read IGNORE
 fi
@@ -19,32 +19,32 @@ fi
 # Create the directory in root home
 cd
 
-rm -r snmon
-mkdir snmon
+rm -r znmon
+mkdir znmon
 
-# Check to see that smartadmin home dir exist
-if [[ ! -d /home/smartadmin ]]
+# Check to see that zenadmin home dir exist
+if [[ ! -d /home/zenadmin ]]
 then
-     echo "Smartadmin directory does not exist. Exiting"
+     echo "zenadmin directory does not exist. Exiting"
      exit -1
 fi
 
 # Create the directory in smartadmin home
-rm -r /home/smartadmin/snmon
-mkdir /home/smartadmin/snmon
+rm -r /home/zenadmin/znmon
+mkdir /home/zenadmin/znmon
 
 # Change the directory to ~/snmon/
-cd ~/snmon/
+cd ~/znmon/
 
 # Download the appropriate scripts
-wget https://rawgit.com/controllinghand/smartnode-monitor/master/snmonagent.sh
+wget https://rawgit.com/controllinghand/securenode-monitor/master/znmonagent.sh
 
 # Create a cronjob for monitoring agent to collect data every 10 minutes
-# dump the results into the smartadmin snmon directory
-# OLD (crontab -l 2>/dev/null | grep -v -F "snmon/snmonagent.sh" ; echo "*/10 * * * * ~/snmon/snmonagent.sh > /home/smartadmin/snmon/snmon.dat 2>&1" ) | crontab -
-(crontab -l 2>/dev/null | grep -v -F "snmon/snmonagent.sh" ; echo "*/10 * * * * ~/snmon/snmonagent.sh" ) | crontab -
-chmod 0700 ./snmonagent.sh
+# dump the results into the zemadmin znmon directory
+# OLD (crontab -l 2>/dev/null | grep -v -F "znmon/zenagent.sh" ; echo "*/10 * * * * ~/znmon/zenagent.sh > /home/zenadmin/znmon/znmon.dat 2>&1" ) | crontab -
+(crontab -l 2>/dev/null | grep -v -F "znmon/znmonagent.sh" ; echo "*/10 * * * * ~/znmon/znmonagent.sh" ) | crontab -
+chmod 0700 ./znmonagent.sh
 
-# Cleanup old installsnma
-rm ~/installsnma.s*
+# Cleanup old installznma
+rm ~/installznma.s*
 
