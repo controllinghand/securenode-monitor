45dc2bf549b8a34f38087a44ee103b9a9f4a7445
controllinghand <s@controllinghand.com>
2018-05-28 14:33:27 -0700

    update to Zen
    From Smartcash to ZenCash

--- installznm.sh
+++ installznm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# installsnm.sh
-# This installs the SmartNode Monitor 
+# installznm.sh
+# This installs the ZenCash SecureNode Monitor 
 # This script runs on your MAC terminal
 
-# Check to see if smartnode monitor directory already exist
-if [ -d ~/snmon ]
+# Check to see if ZenCash securenode monitor directory already exist
+if [ -d ~/znmon ]
 then
-    echo "~/snmon Directory already exist"
+    echo "~/znmon Directory already exist"
     printf "Press Ctrl+C to cancel or Enter to clean up and reinstall:"
     read IGNORE
 fi
@@ -14,21 +14,21 @@ fi
 # Create the directory in home dir location
 cd
 
-rm -r snmon
-mkdir snmon
+rm -r znmon
+mkdir znmon
 
 # Change the directory to ~/snmon/
-cd ~/snmon/
+cd ~/znmon/
 
-# Download the snmon.sh main script
+# Download the znmon.sh main script
 # make sure to issue a ssh-add before you run this script
 # This will let you bypass having to enter a password every time
-curl -O https://raw.githubusercontent.com/controllinghand/smartnode-monitor/master/snmon.sh
-chmod 700 ~/snmon/snmon.sh
+curl -O https://raw.githubusercontent.com/controllinghand/securenode-monitor/master/znmon.sh
+chmod 700 ~/znmon/znmon.sh
 
 # Download the iplist file with example
-# Please edit this file with the IP addresses of all your smartnodes
-curl -O https://raw.githubusercontent.com/controllinghand/smartnode-monitor/master/iplist
+# Please edit this file with the IP addresses of all your securenodes
+curl -O https://raw.githubusercontent.com/controllinghand/securenode-monitor/master/iplist
 
 # Cleanup old installsnm
-rm ~/installsnm.s*
+rm ~/installznm.s*
